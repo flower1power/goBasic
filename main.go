3e6f322b87ecd62e776133b6333ae84eeafd04be
Виктор Лобынцев <sibfight@mail.ru>
2025-06-21 15:33:38 +0300

    функция getUserInput

--- main.go
+++ main.go
@@ -7,21 +7,13 @@ import (
 
 func main() {
 
-	var userHeight float64
-	var userKg float64
-
 	fmt.Println("__Калькулятор индекса массы тела__")
 
-	fmt.Print("Введите свой рост в сантиметрах: ")
-	fmt.Scan(&userHeight)
-
-	fmt.Print("Введите свой вес в кг: ")
-	fmt.Scan(&userKg)
-
-	printResult(calculateIMT(userKg, userHeight))
+	userHeight, userKg := getUserInput()
+	printResult(calculateIMT(userHeight, userKg))
 }
 
-func calculateIMT(kg float64, height float64) float64 {
+func calculateIMT(height float64, kg float64) float64 {
 	const IMTPower = 2
 
 	return kg / math.Pow(height/100, IMTPower)
@@ -29,4 +21,17 @@ func calculateIMT(kg float64, height float64) float64 {
 
 func printResult(IMT float64) {
 	fmt.Printf("Ваш индекс массы тела: %.0f", IMT)
+}
+
+func getUserInput() (height float64, kg float64) {
+	var userHeight float64
+	var userKg float64
+
+	fmt.Print("Введите свой рост в сантиметрах: ")
+	fmt.Scan(&userHeight)
+
+	fmt.Print("Введите свой вес в кг: ")
+	fmt.Scan(&userKg)
+
+	return userHeight, userKg
 }
