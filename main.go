9de25d61f2969cc10c383b7bfad7d3af6d640ddf
Виктор Лобынцев <sibfight@mail.ru>
2025-06-21 14:28:44 +0300

    расчет индекса массы тела

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Printf("Hello")
+	const IMTPower = 2
+
+	var userHeight float64
+	var userKg float64
+
+	fmt.Println("__Калькулятор индекса массы тела__")
+
+	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Scan(&userHeight)
+
+	fmt.Print("Введите свой вес в кг: ")
+	fmt.Scan(&userKg)
+
+	fmt.Print("Ваш индекс массы тела: ")
+	IMT := userKg / math.Pow(userHeight, IMTPower)
+
+	fmt.Println(IMT)
 }
