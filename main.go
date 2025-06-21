1b5ebe33a086ee5e1a9dc730d626312e206133fa
Виктор Лобынцев <sibfight@mail.ru>
2025-06-21 14:34:49 +0300

    форматирование вывода + переход на сантиметры

--- main.go
+++ main.go
@@ -13,14 +13,12 @@ func main() {
 
 	fmt.Println("__Калькулятор индекса массы тела__")
 
-	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Print("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
 
 	fmt.Print("Введите свой вес в кг: ")
 	fmt.Scan(&userKg)
 
-	fmt.Print("Ваш индекс массы тела: ")
-	IMT := userKg / math.Pow(userHeight, IMTPower)
-
-	fmt.Println(IMT)
+	IMT := userKg / math.Pow(userHeight/100, IMTPower)
+	fmt.Printf("Ваш индекс массы тела: %.0f", IMT)
 }
