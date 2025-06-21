83a50748a1f45443a03bf6b54517c39d19528e5c
Виктор Лобынцев <sibfight@mail.ru>
2025-06-21 16:58:28 +0300

    перезапуск и проверка ввода

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
@@ -10,15 +11,50 @@ const IMTPower = 2
 func main() {
 	fmt.Println("__Калькулятор индекса массы тела__")
 
-	printResult(calculateIMT(getUserInput()))
+	for {
+		IMT, err := calculateIMT(getUserInput())
+
+		if err != nil {
+			fmt.Println(err)
+			if !checkRerun() {
+				break
+			}
+			continue
+		}
+
+		printResult(IMT)
+
+		if !checkRerun() {
+			break
+		}
+	}
 }
 
-func calculateIMT(height float64, kg float64) float64 {
-	return kg / math.Pow(height/100, IMTPower)
+func calculateIMT(height float64, kg float64) (float64, error) {
+	if height <= 0 || kg <= 0 {
+		return 0, errors.New("не указан вес или рост")
+	}
+
+	IMT := kg / math.Pow(height/100, IMTPower)
+
+	return IMT, nil
 }
 
 func printResult(IMT float64) {
-	fmt.Printf("Ваш индекс массы тела: %.0f", IMT)
+	fmt.Printf("Ваш индекс массы тела: %.0f \n", IMT)
+
+	switch true {
+	case IMT < 16:
+		fmt.Println("У вас сильный дефицит массы тела")
+	case IMT < 18.5:
+		fmt.Println("У вас дефицит массы тела")
+	case IMT < 25:
+		fmt.Println("Норма")
+	case IMT < 30:
+		fmt.Println("У вас избыточный вес")
+	default:
+		fmt.Println("У вас степень ожирения")
+	}
 }
 
 func getUserInput() (height float64, kg float64) {
@@ -29,4 +65,17 @@ func getUserInput() (height float64, kg float64) {
 	fmt.Scan(&kg)
 
 	return height, kg
+}
+
+func checkRerun() bool {
+	var rerun string
+
+	fmt.Println("Посчитать нового человека? (Y|n)")
+	fmt.Scan(&rerun)
+
+	if rerun == "Y" || rerun == "y" {
+		return true
+	}
+
+	return false
 }
